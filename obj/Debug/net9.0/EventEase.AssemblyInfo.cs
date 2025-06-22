595947074b1c896fe5d36807618c5eab297be1ad
sbcybersolutions <christian@sbcyber.net>
2025-06-22 08:54:13 +0200

    Fixed styling

--- obj/Debug/net9.0/EventEase.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventEase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+872d598543c9aafbb2c7e259e0596eb1743e9787")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
